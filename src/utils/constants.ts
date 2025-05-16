f5018702f7e2fef4d998907ea625753cde396493
shwsrc <fmexicstu@gmail.com>
2025-05-16 16:41:23 +0000

    uirl

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,7 +8,7 @@ export const DISCORD_USER_IDS = [
 export const BACKGROUND_MUSIC_URL = "bc.mp3";
 
 // Background image local path
-export const BACKGROUND_IMAGE_URL = "bc.jpeg";
+export const BACKGROUND_IMAGE_URL = "https://cdn.discordapp.com/attachments/1180707033561976864/1372977181889663026/bc.jpeg?ex=6828bc24&is=68276aa4&hm=c89b93596c7984ef9b220d884e199e88c1b727b5ac094d7aaf9a369237fefe0d&";
 
 // Customize the appearance
 export const THEME = {
